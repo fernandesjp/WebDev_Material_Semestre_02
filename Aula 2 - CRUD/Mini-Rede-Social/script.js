e0262d8274682271264e68a599032520659aa882
f.ernandesjp <labsfiap@fiap.com.br>
2025-08-14 21:14:19 -0300

    alteracoes

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -13,8 +13,62 @@ let posts = [
         date: "12/10/2022 12:00:00"
     },
     {
-        text: "Este é o terceiro post teste",
+        text: "Este é o terceiro post",
         category: "Eventos",
+        image: "https://placedog.net/150?random=4",
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function(){
+   mostrarPosts();
+   document.querySelector("#postForm").addEventListener("submit",addPost)
+
+}
+
+//CREATE
+function addPost (infosDoEvento){
+    infosDoEvento.preventDefault();
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+    const dataPost = new Date().toLocaleString();
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: dataPost
+    }
+    posts.unshift(novoPost);
+
+    mostrarPosts()
+
+}
+
+//READ
+function mostrarPosts(){
+   const listaPosts=document.querySelector("#postList")
+   listaPosts.innerHTML = ""
+// passa em cada item do array criando um tweet
+   posts.forEach(pegaItem => {
+     const cardPost = document.createElement("div")
+     cardPost.classList.add("card")
+
+     cardPost.innerHTML = `
+      <h2>${pegaItem.text}</h2>
+      <img src ="${pegaItem.image}"/>
+      <p>${pegaItem.category}</p>
+      <p>${pegaItem.date}</p>
+       `
+       listaPosts.append(cardPost)
+
+
+    })
+   
+}
+
+//UPDATE
+function editarPosts(){}
+//DELETE
+function deletarPosts (){}
