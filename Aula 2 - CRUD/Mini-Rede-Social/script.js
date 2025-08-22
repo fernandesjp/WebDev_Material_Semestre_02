5e8bc6277a756096c6853c61097959d0c277addc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 21:32:28 -0300

    aula 21/08

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -13,62 +13,120 @@ let posts = [
         date: "12/10/2022 12:00:00"
     },
     {
-        text: "Este é o terceiro post",
+        text: "Este é o terceiro post teste",
         category: "Eventos",
-        image: "https://placedog.net/150?random=4",
+        image: "https://placedog.net/150?random=3",
         date: "12/10/2023 12:00:00"
     }
 ];
-
+ 
 window.onload = function(){
-   mostrarPosts();
-   document.querySelector("#postForm").addEventListener("submit",addPost)
+    carregarPosts();
+    mostrarPost();
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+ 
+    document.querySelector('#postList').addEventListener('click', handleClick)
 
+    //localStorage.setItem("nome","Fulano")
+    //console.log(localStorage.getItem("nome"));
+    //localStorage.removeItem("nome");
+    //localStorage.clear();
 }
+ 
+function handleClick(infosDoEvento){
+ 
+    const action = infosDoEvento.target.dataset.action;
+    const index = infosDoEvento.target.dataset.index;
 
+    if(action === "Editar"){
+        editarPost(index)
+    }
+    else if(action === "Apagar"){
+        deletarPost(index)
+    }
+}
+ 
 //CREATE
-function addPost (infosDoEvento){
-    infosDoEvento.preventDefault();
-    const textoPost = document.querySelector("#postText").value;
-    const categoriaPost = document.querySelector("#postCategory").value;
-    const imagemPost = document.querySelector("#postImage").value;
-    const dataPost = new Date().toLocaleString();
-
+function addPost(infosDoEvento){
+    infosDoEvento.preventDefault()
+ 
+    //cria variaveis para todos os posts
+    const textoPost = document.querySelector("#postText").value
+    const categoriaPost = document.querySelector("#postCategory").value
+    const imagemPost = document.querySelector("#postImage").value
+    const dataPost = new Date().toLocaleString()
+   
+    // pega o array de cima e junta com o novoPost
     const novoPost = {
         text: textoPost,
         category: categoriaPost,
         image: imagemPost,
-        date: dataPost
+        date: dataPost,
     }
-    posts.unshift(novoPost);
-
-    mostrarPosts()
-
+ 
+    //deixa o post do mais novo para o mais antigo por conta do unshift
+    posts.unshift(novoPost)
+    salvarPosts();
+ 
+    document.querySelector('#postForm').reset()
+ 
+ 
+    //chama a função mostrarPost para mostrar o novo post
+    mostrarPost()
 }
-
 //READ
-function mostrarPosts(){
-   const listaPosts=document.querySelector("#postList")
-   listaPosts.innerHTML = ""
-// passa em cada item do array criando um tweet
-   posts.forEach(pegaItem => {
-     const cardPost = document.createElement("div")
-     cardPost.classList.add("card")
-
-     cardPost.innerHTML = `
-      <h2>${pegaItem.text}</h2>
-      <img src ="${pegaItem.image}"/>
-      <p>${pegaItem.category}</p>
-      <p>${pegaItem.date}</p>
-       `
-       listaPosts.append(cardPost)
-
-
+function mostrarPost(){
+    //pega a id postList
+    const listaPosts = document.querySelector("#postList")
+    listaPosts.innerHTML = ""
+ 
+    //passa nos items do array
+    posts.forEach((pegaItem, index) => {
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+ 
+        cardPost.innerHTML = `
+        <h2>${pegaItem.text}</h2>
+        <img src ="${pegaItem.image}"/>
+        <p>${pegaItem.category}</p>
+        <p>${pegaItem.date}</p>
+        <button data-action="Editar" data-index="${index}"><i class="fa-solid fa pen-to-square"></i>
+        Editar</button>
+        <button data-action="Apagar" data-index="${index}"><i class="fa-solid fa pen-to-square"></i>
+        Apagar</button>
+        `
+ 
+        //adiciona o post
+        listaPosts.append(cardPost)
+ 
     })
-   
+}
+//UPDATE
+function editarPost(index){
+  const novoTexto = prompt('Edite o conteúdo do post',posts[index].text);
+  if(confirmar){
+  posts[index].text = novoTexto;
+}
+  mostrarPost();
+}
+//DELETE
+function deletarPost(index){
+  const confirmar = confirm("Você deseja realmente excluir esse post?");
+  if(confirmar){
+  posts.splice(index,1);
+  salvarPosts();
+}
+  mostrarPost();
 }
 
-//UPDATE
-function editarPosts(){}
-//DELETE
-function deletarPosts (){}
+function salvarPosts(){
+    localStorage.setItem("posts", JSON.stringify(posts));
+}
+
+function carregarPosts(){
+    const postsGuardados = localStorage.getItem("posts");
+    if(postsGuardados){
+        posts = JSON.parse(postsGuardados)
+    }
+}
+        
